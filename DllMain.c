c7e54aa554b0c2c80b89855addacc35189be28ad
gerald-lindsly <gerald.lindsly@gmail.com>
2011-10-27 07:04:06 -0400

    fix DllMain for linux

--- DllMain.c
+++ DllMain.c
@@ -1,17 +1,29 @@
+#ifdef _WIN32
 #include <windows.h>
+#endif
+
 #include "bson.h"
 #include <mex.h>
 
 extern int sock_init();
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-                      DWORD  ul_reason_for_call,
-                      LPVOID lpReserved)
+
+int main(void)
 {
     sock_init();
     bson_printf = mexPrintf;
     bson_errprintf = mexPrintf;
     set_bson_err_handler(mexErrMsgTxt);
+    return 0;
+}
+
+#ifdef _WIN32
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved)
+{
+    main();
     return TRUE;
 }
+#endif
 
